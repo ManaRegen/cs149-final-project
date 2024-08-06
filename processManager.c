fcacb208d59b6464a62ccb9411d192c8c413743b
mana <vuvictor2003@gmail.com>
2024-08-05 20:14:18 -0700

    reformat for readability

--- processManager.c
+++ processManager.c
@@ -21,72 +21,77 @@ PcbEntry pcbTable[99];
 static void blockProcess();
 static void reporterProcess();
 
-void processManager(int command_fd[2], int response_fd[2]) {
-    //Initialize the queue
+void processManager(int command_fd[2], int response_fd[2])
+{
+    // Initialize the queue
     initializeQueue(&readyState);
 
-    //Loop to add all processes to the readyState queue
+    // Loop to add all processes to the readyState queue
     for (int i = 0; i < 99; i++)
     {
         enqueue(&readyState, pcbTable[i].processId);
     }
 
-    close(command_fd[1]); // Close unused write end
+    close(command_fd[1]);  // Close unused write end
     close(response_fd[0]); // close unused read end
     char command;
     char signal;
-    
-    while (true) {
+
+    while (true)
+    {
         read(command_fd[0], &command, 1);
-        switch (command) {
-            case 'Q':
-                q();
-                break;
-            case 'U':
-                // Unblock the first process in the blocked queue
-                u();
-                break;
-            case 'P':
-                p();
-                break;
-            case 'T':
-                // Print average turnaround time and terminate the system
-                int avgTurnaround;
-                if (completedProcessesCount == 0) {
-                    avgTurnaround = 0;
-                } else {
-                    avgTurnaround = totalTurnaround / completedProcessesCount;
-                }
+        switch (command)
+        {
+        case 'Q':
+            q();
+            break;
+        case 'U':
+            // Unblock the first process in the blocked queue
+            u();
+            break;
+        case 'P':
+            p();
+            break;
+        case 'T':
+            // Print average turnaround time and terminate the system
+            int avgTurnaround;
+            if (completedProcessesCount == 0)
+            {
+                avgTurnaround = 0;
+            }
+            else
+            {
+                avgTurnaround = totalTurnaround / completedProcessesCount;
+            }
 
-                printf("Average turnaround time: %d\n", avgTurnaround);
-                printf("Terminating system.\n");
+            printf("Average turnaround time: %d\n", avgTurnaround);
+            printf("Terminating system.\n");
 
-                close(command_fd[0]);
-                close(response_fd[1]);
+            close(command_fd[0]);
+            close(response_fd[1]);
 
-                signal = 'T';
-                write(response_fd[1], &signal, sizeof(char));
-                return;
-            default:
-                printf("Invalid command. Please try again.\n");
-                break;
+            signal = 'T';
+            write(response_fd[1], &signal, sizeof(char));
+            return;
+        default:
+            printf("Invalid command. Please try again.\n");
+            break;
         }
         signal = 1;
         write(response_fd[1], &signal, sizeof(char));
     }
-
 }
 
-static void runProcess() { // selects process from the top of the readyState queue to run
-    //Update runningState and readyState
+static void runProcess()
+{ // selects process from the top of the readyState queue to run
+    // Update runningState and readyState
     if (!isEmpty(&readyState))
     {
         runningState = readyState.front;
         dequeue(&readyState);
     }
-    
 }
 
-static void blockProcess() {
-
+static void blockProcess()
+{
 }
--- q.c
+++ q.c
@@ -1,21 +1,25 @@
 #include "q.h"
-#include "queue.h" // Include for Queue related operations
+#include "queue.h"   // Include for Queue related operations
 #include "structs.h" // Include for PcbEntry
 
 extern Queue readyState;
 extern int runningState;
 
-void runProcess() { // selects process from the top of the readyState queue to run
+void runProcess()
+{ // selects process from the top of the readyState queue to run
     // Update runningState and readyState
-    if (!isEmpty(&readyState)) {
+    if (!isEmpty(&readyState))
+    {
         runningState = readyState.front;
         dequeue(&readyState);
     }
 }
 
-void q() {
+void q()
+{
     // Execute the next instruction of the running process
-    if (runningState == -1) {
+    if (runningState == -1)
+    {
         runProcess();
     }
 }
