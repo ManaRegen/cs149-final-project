4a6271b15a60e74c5d5b05882d053ec67066a67e
mana <vuvictor2003@gmail.com>
2024-08-06 19:18:54 -0700

    Create header file for instructions.c

---
+++ headers/instructions.h
@@ -0,0 +1,26 @@
+#ifndef INSTRUCTIONS_H
+#define INSTRUCTIONS_H
+
+#include "queue.h"
+#include "structs.h"
+
+
+// CPU structure
+extern Cpu cpu;
+
+// External variables
+extern int runningState;
+extern Queue blockedState;
+extern PcbEntry pcbTable[99];
+extern int time;
+
+// Function prototypes
+void S(int n);    // Set CPU value to n
+void A(int n);    // Add n to CPU value
+void D(int n);    // Subtract n from CPU value
+void B();         // Block the running process
+void E();         // Terminate the process
+void F();         // Create a new process
+void R(char filename); // Replace the program of the process with filename
+
+#endif // INSTRUCTIONS_H
