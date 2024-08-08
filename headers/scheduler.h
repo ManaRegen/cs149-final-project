18884de557858085e79872eeaaf573248a479a8d
mana <vuvictor2003@gmail.com>
2024-08-08 15:28:23 -0700

    Remove u.c dependency on queue.c in order to modify behavior

---
+++ headers/scheduler.h
@@ -0,0 +1,12 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include "../headers/globals.h"
+#include "../headers/processController.h"
+#include "../headers/queue.h"
+
+void scheduleProcess();
+
+#endif // SCHEDULER_H
---
+++ src/scheduler.c
@@ -0,0 +1,51 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include "../headers/globals.h"
+#include "../headers/processController.h"
+#include "../headers/queue.h"
+
+static int getHighestPriorityProcess()
+{
+    int pid;
+
+    pid = dequeueIfNotEmpty(&readyState[3]);
+    if (pid != -1)
+    {
+        return pid;
+    }
+
+    pid = dequeueIfNotEmpty(&readyState[2]);
+    if (pid != -1)
+    {
+        return pid;
+    }
+
+    pid = dequeueIfNotEmpty(&readyState[1]);
+    if (pid != -1)
+    {
+        return pid;
+    }
+
+    pid = dequeueIfNotEmpty(&readyState[0]);
+    if (pid != -1)
+    {
+        return pid;
+    }
+
+    return -1;
+}
+
+static int dequeueIfNotEmpty(Queue *queue)
+{
+    if (!isEmpty(queue))
+    {
+        return dequeue(queue);
+    }
+    return -1;
+}
+
+void scheduleProcess()
+{
+    int highestPriorityProcess = getHighestPriorityProcess();
+    loadContext(highestPriorityProcess);
+}
